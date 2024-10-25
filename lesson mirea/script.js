b6aab8c8f32331d601711e5c6fe2c7a880b9e095
m1sha <Mihaelllo.man@gmail.com>
2024-10-25 17:15:27 +0300

    bjkvbskjvb

---
+++ lesson mirea/script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Иванов Михаил Валерьевич";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
